afe0ee06eef4fa4bf3da6403ab4d6e33d1ad0e36
Andrew Phillips <demobox1@yahoo.co.uk>
2015-08-21 17:52:40 -0400

    Cleaning up the implementation of LoadVariables and StoreVariables

--- src/main/resources/vars/StoreVariables.py
+++ src/main/resources/vars/StoreVariables.py
@@ -3,40 +3,28 @@
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
 # FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
 #
-import os, sys, time
+import sys
 
-import com.xebialabs.xlrelease.domain.Release
 import com.xhaus.jyson.JysonCodec as json
 
-from java.lang import String
-from java.util import Arrays
-from org.apache.commons.collections.list import FixedSizeList;
-import urllib
-
-__release = getCurrentRelease()
-
-
-
-def get_variable_store(variable_store):
-
-    request = HttpRequest({'url': 'http://localhost:5516'}, release.scriptUsername, release.scriptUserPassword)
-
-    response = request.get('/configurations', contentType='application/json')
+def update_variable_store(variable_store_title, variables):
+    conn_fac = HttpRequest({'url': 'http://localhost:5516'}, release.scriptUsername, release.scriptUserPassword)
+    response = conn_fac.get('/configurations', contentType = 'application/json')
     data = json.loads(response.getResponse())
 
+    variable_store_ci = None
     for i in range(0, len(data)):
-        if variable_store['title'] == data[i]['properties']['title'] and variable_store['variable_json'] == \
-                data[i]['properties']['variable_json']:
-                return data[i]
+        if data[i]['type'] == 'vars.VariableStore' and variable_store_title == data[i]['properties']['title']:
+                variable_store_ci = data[i]
+                break
+    if not variable_store_ci:
+        print "ERROR: Unable to find variable store '%s'" % (variable_store_title)
+        sys.exit(1)
+    variable_store_ci['properties']['variablesJson'] = json.dumps(variables)
+    response = conn_fac.put('/configurations/%s' % (variable_store_ci['id']), json.dumps(variable_store_ci), contentType = 'application/json')
+    if not response.isSuccessful:
+        print "ERROR: Unable to update variable store '%s':" % (variable_store_title)
+        response.errorDump()
+        sys.exit(1)
 
-def save_variable_store(config_item, variables):
-    config_item['properties']['variable_json'] = json.dumps(variables)
-
-    jsonPayload=json.dumps(config_item)
-    request = HttpRequest({'url': 'http://localhost:5516'}, release.scriptUsername, release.scriptUserPassword)
-    response = request.put('/configurations/%s' % (config_item['id']), jsonPayload,contentType = 'application/json')
-
-
-save_variable_store(get_variable_store(variableStore), getCurrentRelease().getVariableValues())
-
-sys.exit(0)
+update_variable_store(variableStore['title'], getCurrentRelease().getVariableValues())
